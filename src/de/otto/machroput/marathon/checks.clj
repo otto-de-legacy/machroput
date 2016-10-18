ef105ae5f19a25cfc62cd166834bfc26c395f93a
Kai Brandes <kai.brandes@otto.de>
2016-10-18 13:52:26 +0200

    destructuring in one-line for checks

--- src/de/otto/machroput/marathon/checks.clj
+++ src/de/otto/machroput/marathon/checks.clj
@@ -26,19 +26,15 @@
                             (format "Version Check NOT ok! Found version %s on status page and not %s" current-version expected-version))))
 
 (defn marathon-task-health-check [mconn {:keys [print-fn]} {:keys [id instances]}]
-  (let [app-conf (mc/get-app mconn id)
-        tasks-running (get-in app-conf [:app :tasksRunning])
-        tasks-healthy (get-in app-conf [:app :tasksHealthy])
-        tasks-unhealth (get-in app-conf [:app :tasksUnhealthy])]
+  (let [{{:keys [tasksUnhealthy tasksHealthy tasksRunning]} :app} (mc/get-app mconn id)]
     (false-check-and-return print-fn
-                            (and (= tasks-running instances)
-                                 (= tasks-healthy tasks-running)
-                                 (= 0 tasks-unhealth))
-                            (format "Task Check was NOT ok! running: %s healthy: %s unhealthy: %s" tasks-running tasks-healthy tasks-unhealth))))
+                            (and (= tasksRunning instances)
+                                 (= tasksHealthy tasksRunning)
+                                 (= 0 tasksUnhealthy))
+                            (format "Task Check was NOT ok! running: %s healthy: %s unhealthy: %s" tasksRunning tasksHealthy tasksUnhealthy))))
 
 (defn marathon-app-version-check [mconn {:keys [print-fn]} {:keys [id marathon-deploy-version]}]
-  (let [current-app-status (mc/get-app mconn (:id id))
-        current-app-version (get-in current-app-status [:app :version])]
+  (let [{{current-app-version :version} :app} (mc/get-app mconn (:id id))]
     (false-check-and-return print-fn
                             (= current-app-version marathon-deploy-version)
                             (format "Marathon-Deploy-Version Check was NOT ok! App is not running latest deployment-version %s: %s" marathon-deploy-version current-app-version))))
