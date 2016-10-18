073a53eb602550f7c357545cf8770dfe3a6fdd43
Kai Brandes <kai.brandes@otto.de>
2016-10-18 13:55:55 +0200

    rename check helper-fns

--- src/de/otto/machroput/marathon/checks.clj
+++ src/de/otto/machroput/marathon/checks.clj
@@ -7,12 +7,12 @@
   (with-marathon-app-version-check [self])
   (with-deployment-stopped-check [self]))
 
-(defn false-check-and-return [print-fn cond msg]
+(defn- fcheck [print-fn cond msg]
   (when (not cond)
     (print-fn msg))
   cond)
 
-(defn get-current-version! [print-fn app-version-fn]
+(defn- get-current-version! [print-fn app-version-fn]
   (try
     (app-version-fn)
     (catch Exception e
@@ -21,25 +21,26 @@
 
 (defn app-version-check [_ {:keys [print-fn app-version-fn]} {expected-version :version}]
   (let [current-version (get-current-version! print-fn app-version-fn)]
-    (false-check-and-return print-fn
-                            (= current-version expected-version)
-                            (format "Version Check NOT ok! Found version %s on status page and not %s" current-version expected-version))))
+    (fcheck print-fn
+            (= current-version expected-version)
+            (format "Version Check NOT ok! Found version %s on status page and not %s" current-version expected-version))))
 
 (defn marathon-task-health-check [mconn {:keys [print-fn]} {:keys [id instances]}]
   (let [{{:keys [tasksUnhealthy tasksHealthy tasksRunning]} :app} (mc/get-app mconn id)]
-    (false-check-and-return print-fn
-                            (and (= tasksRunning instances)
-                                 (= tasksHealthy tasksRunning)
-                                 (= 0 tasksUnhealthy))
-                            (format "Task Check was NOT ok! running: %s healthy: %s unhealthy: %s" tasksRunning tasksHealthy tasksUnhealthy))))
+    (fcheck print-fn
+            (and (= tasksRunning instances)
+                 (= tasksHealthy tasksRunning)
+                 (= 0 tasksUnhealthy))
+            (format "Task Check was NOT ok! running: %s healthy: %s unhealthy: %s" tasksRunning tasksHealthy tasksUnhealthy))))
 
 (defn marathon-app-version-check [mconn {:keys [print-fn]} {:keys [id marathon-deploy-version]}]
   (let [{{current-app-version :version} :app} (mc/get-app mconn (:id id))]
-    (false-check-and-return print-fn
-                            (= current-app-version marathon-deploy-version)
-                            (format "Marathon-Deploy-Version Check was NOT ok! App is not running latest deployment-version %s: %s" marathon-deploy-version current-app-version))))
+    (fcheck print-fn
+            (= current-app-version marathon-deploy-version)
+            (format "Marathon-Deploy-Version Check was NOT ok! App is not running latest deployment-version %s: %s" marathon-deploy-version current-app-version))))
 
 (defn deployment-stopped-check [mconn {:keys [print-fn]} {:keys [marathon-deploy-version]}]
-  (false-check-and-return print-fn
-                          (= false (mc/deployment-still-running? mconn marathon-deploy-version))
-                          "Marathon-Deployment Check was NOT ok! The started Marathon-deployment is still running"))
+  (fcheck print-fn
+          (= false (mc/deployment-still-running? mconn marathon-deploy-version))
+          "Marathon-Deployment Check was NOT ok! The started Marathon-deployment is still running"))
+
