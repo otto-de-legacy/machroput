94180aaf32bdeb7d21ae3b4f92b0a677baa460bc
Kai Brandes <kai.brandes@otto.de>
2016-10-18 14:48:24 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject de.otto/machroput "0.2.0"
   :description "A simple marathon and chronos api written in clojure"
   :url "https://github.com/otto-de/machroput.git"
-  :dependencies [[http-kit "2.1.19"]
+  :dependencies [[http-kit "2.2.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.clojure/clojure "1.7.0"]]
+                 [org.clojure/clojure "1.8.0"]]
   :plugins [[lein-ancient "0.6.7"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
