864459e40792af2a3588668e93e1c7e6c3112e2b
Kai Brandes <kai.brandes@otto.de>
2016-10-18 16:15:00 +0200

    rollback http-kit version since it lead to problems

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject de.otto/machroput "1.0.0"
+(defproject de.otto/machroput "1.0.1"
   :description "A simple marathon and chronos api written in clojure"
   :url "https://github.com/otto-de/machroput.git"
-  :dependencies [[http-kit "2.2.0"]
+  :dependencies [[http-kit "2.1.19"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/clojure "1.8.0"]]
   :plugins [[lein-ancient "0.6.7"]]
