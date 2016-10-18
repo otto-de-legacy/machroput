09c7a786e9a6a00ffa1f268ba7c3699b245c06f4
carl.duevel <carl.duevel@holisticon.de>
2016-10-18 21:25:18 +0200

    Added release config

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject de.otto/machroput "1.0.1"
+(defproject de.otto/machroput "1.0.2-SNAPSHOT"
   :description "A simple marathon and chronos api written in clojure"
   :url "https://github.com/otto-de/machroput.git"
   :dependencies [[http-kit "2.2.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/clojure "1.8.0"]]
+  :lein-release {:deploy-via :clojars}
   :plugins [[lein-ancient "0.6.10"]]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:plugins [[lein-release/lein-release "1.0.9"]]}})
